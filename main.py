9c52e6230d315f193aa0e0f1e1980ba422372d8b
Nevena Ikonic <ikonicnena@gmail.com>
2025-02-18 19:45:25 +0100

    Data Clean Up

--- main.py
+++ main.py
@@ -2,5 +2,24 @@ import pandas as pd
 
 data = pd.read_csv('data/creditcard_modify.csv')
 
-print(data.head())
-print(data.columns)
+# n = data.shape
+# print(n)
+
+# rows = n[0]
+# cols = n[1]
+
+# print(data.head())
+# #print(data.columns)
+
+# print(data.dtypes)
+
+
+data_numeric = data.apply(pd.to_numeric, errors='coerce')
+
+clean_data = data_numeric.dropna()
+#print(clean_data)
+
+#non_numeric_rows = data_numeric[data_numeric.isna().any(axis=1)]
+#print(non_numeric_rows)
+
+
