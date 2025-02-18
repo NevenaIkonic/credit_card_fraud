074b463c870bde0edcb168e7ca260ddb1b6c053f
Nevena Ikonic <ikonicnena@gmail.com>
2025-02-18 20:28:46 +0100

    plotting

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+
 
 data = pd.read_csv('data/creditcard_modify.csv')
 
@@ -21,5 +23,21 @@ clean_data = data_numeric.dropna()
 
 #non_numeric_rows = data_numeric[data_numeric.isna().any(axis=1)]
 #print(non_numeric_rows)
+
+
+
+data['Time'].plot(kind='hist', bins=100, figsize=(14, 6))
+plt.show()
+
+
+for i in range (1,29):
+    data[f'V{i}'].plot(kind='hist', bins=100, figsize=(14,6))
+    plt.show()
+
+data['Amount'].plot(kind='hist', bins=100, figsize=(14, 6))
+plt.show()
+
+data['Class'].plot(kind='hist', bins=100, figsize=(14, 6))
+plt.show()
 
 
